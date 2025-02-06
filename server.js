ef1d05b570857d7dcf1661759c63fd226a98fb02
Arsalan <arsalanmir235@gmail.com>
2025-02-06 15:31:27 +0530

    added router

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import express from 'express'
+import express from 'express';
+import AuthRouter from "./routes/auth.route.js"
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());// middleware
 
 app.get('/',(req, res)=>{
   res.send("server is working")
 
 });
+app.use("/auth",AuthRouter)
 
 app.listen(PORT,()=>{
     console.log(`server is running on this.. ${PORT}`)
-})
+});
